9fcb1f1a354978c14a0fc2efe0427753a227edf4
Darcy McBeth <jackmcbeth28469@gmail.com>
2023-08-04 12:17:55 +0100

    Initial commit.
    C# is broken on VS code right now so can't do much more work. The compiler crashes every time. Tried to commit this through bash but for some reason Git doesn't want to commit here.

---
+++ Lesson 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Hello World";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+}
---
+++ Lesson 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using system;
+
+// Hello World! program
+namespace HelloWorld
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+            var name;
+            // Setting styling.
+            Console.Title = "Variables";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            // Actual Code.
+            Console.WriteLine("What is your name?");
+            while (isRunning)
+            {
+                name = Console.ReadLine();
+                if (name is string)
+                {
+                    isRunning = false;
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid name.");
+                }
+            }
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
